2c0fd49fc8f5ff20a90f2f51b79d650b284fd11e
vljh246v <vljh246v@naver.com>
2024-06-24 18:36:25 +0900

    first init

---
+++ src/main/kotlin/com/demo/tourwave/TourwaveApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.tourwave
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TourwaveApplication
+
+fun main(args: Array<String>) {
+    runApplication<TourwaveApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/tourwave/TourwaveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.tourwave
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TourwaveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
